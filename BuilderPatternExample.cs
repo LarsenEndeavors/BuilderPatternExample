a7adeb9708eac14926d15e9d59830a1461defe11
skathix <32309102+skathix@users.noreply.github.com>
2021-10-07 21:40:15 -0500

    Boilerplate added

--- BuilderPatternExample.cs
+++ BuilderPatternExample.cs
@@ -5,6 +5,17 @@ namespace BuilderPatternExample
 {
     internal static class BuilderPatternExample
     {
+        /*
+        * The majority of this code is mocked or directly taken from the Derek Banas YouTube series in
+        * an effort to teach myself and get familiar with these patterns.  None of this is meant to be
+        * original content, and if you see this and wonder why it's in my repo, mostly it's for me, but
+        * I'm happy that you're here and here's proof that I have at least heard of this particular
+        * pattern!
+        *
+        * Author: Nicholas Larsen
+        * Date: 2021/10/07
+        */
+        
         private static void Main()
         {
             // This is the crux of the builder pattern, you create an object that implements an interface
