a2d974ee0eaa6494198f50f0bf5c12e07c92c0d8
skathix <32309102+skathix@users.noreply.github.com>
2021-10-07 21:16:45 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BuilderPatternExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
